0d246d0590534134055f7d0321bc8e0eb57bce4b
Carlos Daniel <86665231+Hartye@users.noreply.github.com>
2021-08-22 16:18:37 +0100

    Add files via upload

---
+++ Array.from and Classes/index.js
@@ -0,0 +1,24 @@
+class dateClass {
+    constructor() {
+
+        this.result()
+    }
+
+    result(start, stop) {
+        stop = stop === undefined ? stop = start: stop = stop
+        start = stop === start ? start = 0: start = start - 1
+
+        const finalResult = Array.from({length: stop - start}, (_, i) => {
+            const month = start + i
+            const date = new Date(0, month)
+
+            return date
+        })
+
+        console.log(finalResult.map(x => x.toLocaleString("pt-BR", { month: "long" })))
+    }
+}
+
+const dateOnClass = new dateClass()
+
+console.log(dateOnClass.result(2))
